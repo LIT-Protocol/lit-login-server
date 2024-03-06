cf829a403bd879a821f01c5a35c6436ba7f92a3a
FedericoAmura <federicoamura@gmail.com>
2024-03-06 20:00:23 -0300

    feat: add caller handling to enable running the oauth login flow in a modal instead of using redirects

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,20 @@
+function parseLoginParams(url) {
+  const searchParams = new URLSearchParams(new URL(url).search);
+  const caller = searchParams.get('caller');
+  const provider = searchParams.get('provider');
+  const accessToken = searchParams.get('access_token');
+  const idToken = searchParams.get('id_token');
+  const error = searchParams.get('error');
+
+  return {
+    caller,
+    provider,
+    accessToken,
+    idToken,
+    error,
+  };
+}
+
 window.onload = async () => {
   // Style other pages
   if (
@@ -6,5 +23,16 @@ window.onload = async () => {
   ) {
     lightMode = false;
     document.body.setAttribute('data-lit-theme', 'dark');
+  }
+
+  // Sending oauth data back to the caller window
+  const openerWindow = window.opener;
+  if (openerWindow) {
+    const params = parseLoginParams(window.location.href);
+    const { caller, error, idToken, access_token, provider } = params
+
+    if (caller) {
+      openerWindow.postMessage({ error, token: idToken || access_token, provider }, caller);
+    }
   }
 };
